3f8f90382607f72f4b5947688212f6b4ddb2ff1e
GabrielCabrera <cabreraburgosgabriel@gmail.com>
2024-05-02 21:48:11 -0600

    Añadir la hora en tiempo real

--- yes_no_app/lib/presentation/widgets/chat/her_message_bubble.dart
+++ yes_no_app/lib/presentation/widgets/chat/her_message_bubble.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class HerMessageBubble extends StatelessWidget {
   final Message message;
-  const HerMessageBubble({super.key, required this.message});
+  
+  const HerMessageBubble({super.key, required this.message,});
 
   @override
   Widget build(BuildContext context) {
@@ -36,30 +38,46 @@ final colors = Theme.of(context).colorScheme;
 
 class _ImageBubble extends StatelessWidget {
   final String imageUrl;
-  const _ImageBubble (this.imageUrl);
+  const _ImageBubble(this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
-
     final size = MediaQuery.of(context).size;
+    final currentTime = DateTime.now(); // Obtener la hora actual
 
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: Image.network(
-      imageUrl,
-      width: size.width * 0.7,
-      height: 150,
-      fit: BoxFit.cover,
-      loadingBuilder: (context, child, loadingProgress){
-        
-        if(loadingProgress == null) return child;
-        return Container(
-          width: size.width * 0.7,
-          height: 150,
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-          child: const Text('Mi novia está enviando una imagen'),
-        );
-      }
-      ));
+    // Formatear los minutos con un cero delante si es un solo dígito
+    final formattedMinutes = currentTime.minute.toString().padLeft(2, '0');
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.network(
+            imageUrl,
+            width: size.width * 0.7,
+            height: 180,
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress == null) return child;
+              return Container(
+                width: size.width * 0.7,
+                height: 150,
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: const Text('Mi novia está enviando una imagen'),
+              );
+            },
+          ),
+        ),
+        const SizedBox(height: 5),
+        Padding(
+          padding: const EdgeInsets.only(right: 10), // Ajustar el espacio a la derecha
+          child: Text(
+            '${currentTime.hour}:$formattedMinutes',
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+      ],
+    );
   }
 }
--- yes_no_app/lib/presentation/widgets/chat/my_message_bubble.dart
+++ yes_no_app/lib/presentation/widgets/chat/my_message_bubble.dart
@@ -2,29 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class MyMessageBubble extends StatelessWidget {
-  const MyMessageBubble({super.key, required this.message});
+  const MyMessageBubble({Key? key, required this.message}) : super(key: key);
   final Message message;
 
   @override
   Widget build(BuildContext context) {
-
-    
-final colors = Theme.of(context).colorScheme;
+    final colors = Theme.of(context).colorScheme;
+    final currentTime = DateTime.now(); // Obtener la hora actual
+    final formattedMinutes = currentTime.minute.toString().padLeft(2, '0'); // Formatear los minutos con un cero delante si es un solo dígito
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.end,
       children: [
-        Container( 
-        decoration: BoxDecoration(
-        color: colors.primary, borderRadius: BorderRadius.circular(20)),
-        child: Padding(
-          padding:  const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Text(message.text, 
-          style: const TextStyle(color: Colors.white),
+        Container(
+          decoration: BoxDecoration(
+            color: colors.primary,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            child: Text(
+              message.text,
+              style: const TextStyle(color: Colors.white),
+            ),
           ),
         ),
+        const SizedBox(height: 10),
+        Padding(
+          padding: const EdgeInsets.only(right: 10, bottom: 30), // Ajustar el espacio a la derecha
+          child: Text(
+            '${currentTime.hour}:$formattedMinutes',
+            style: TextStyle(color: Colors.grey),
+          ),
         ),
-       const SizedBox(height: 10)
       ],
     );
   }
